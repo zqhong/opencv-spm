1af9f47552ef2ba6a1b40d343abb87a7ae223ab6
zqhong <zqh0513@gmail.com>
2025-07-08 17:20:32 +0800

    updated: opencv 4.12.0 for macOS amd64

---
+++ Package.swift
@@ -0,0 +1,37 @@
+
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let version = "4.12.0"
+let checksum = "0087d3a1ee38b793db5fa780039777869cbe764e018bb6d148bf95ea2a3655e2"
+
+let package = Package(
+    name: "OpenCV",
+    platforms: [
+        .macOS(.v14_0)
+    ],
+    products: [
+        .library(
+            name: "OpenCV",
+            targets: ["opencv2", "opencv2-dependencies"]),
+    ],
+    targets: [
+        .binaryTarget(name: "opencv2",
+                      url: "https://github.com/zqhong/opencv-spm/releases/download/\(version)/opencv2.xcframework.zip",
+                      checksum: checksum),
+        .target(
+            name: "opencv2-dependencies",
+            linkerSettings: [
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("CoreImage"),
+                .linkedFramework("CoreMedia"),
+                .linkedFramework("CoreVideo", .when(platforms: [.iOS, .visionOS])),
+                .linkedFramework("Accelerate", .when(platforms: [.iOS, .macOS, .visionOS])),
+                .linkedFramework("OpenCL", .when(platforms: [.macOS])),
+                .linkedLibrary("c++")
+            ]
+        )
+    ]
+)
