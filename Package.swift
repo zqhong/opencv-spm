990ec7f95b6fdc9caf7cebd88cbd8fbf5a59114c
zqhong <zqh0513@gmail.com>
2025-07-08 17:33:32 +0800

    Update Package.swift:  macOS 10.14，开始 OpenCL 已弃用

--- Package.swift
+++ Package.swift
@@ -1,7 +1,4 @@
-
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let version = "4.12.0"
@@ -15,21 +12,22 @@ let package = Package(
     products: [
         .library(
             name: "OpenCV",
-            targets: ["opencv2", "opencv2-dependencies"]),
+            targets: ["opencv2", "opencv2-dependencies"]
+        )
     ],
     targets: [
-        .binaryTarget(name: "opencv2",
-                      url: "https://github.com/zqhong/opencv-spm/releases/download/\(version)/opencv2.xcframework.zip",
-                      checksum: checksum),
+        .binaryTarget(
+            name: "opencv2",
+            url: "https://github.com/zqhong/opencv-spm/releases/download/\(version)/opencv2.xcframework.zip",
+            checksum: checksum
+        ),
         .target(
             name: "opencv2-dependencies",
             linkerSettings: [
                 .linkedFramework("AVFoundation"),
                 .linkedFramework("CoreImage"),
                 .linkedFramework("CoreMedia"),
-                .linkedFramework("CoreVideo", .when(platforms: [.iOS, .visionOS])),
-                .linkedFramework("Accelerate", .when(platforms: [.iOS, .macOS, .visionOS])),
-                .linkedFramework("OpenCL", .when(platforms: [.macOS])),
+                .linkedFramework("Accelerate"),
                 .linkedLibrary("c++")
             ]
         )
